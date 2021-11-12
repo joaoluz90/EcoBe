431b3194b533292b2426495e76863c657a4240de
João Luz <joaoluz900@gmail.com>
2021-11-12 00:32:08 +0000

    connectio bd online

--- models/connection.js
+++ models/connection.js
@@ -3,10 +3,10 @@ var util = require('util')
 
 var pool  = mysql.createPool({
     connectionLimit : 10,
-    host     : 'localhost',
-    user     : 'root',
-    password : 'mysqlpassword',
-    database : 'EcoBe'
+    host     : 'db4free.net',
+    user     : 'joaoluz900',
+    password : 'minhoca011',
+    database : 'eco_be'
 });
 
 pool.query = util.promisify(pool.query)
