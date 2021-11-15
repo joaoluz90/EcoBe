4858ab7efe0c5fd647c765213a88804dfbe97cf5
João Luz <joaoluz900@gmail.com>
2021-11-15 21:13:37 +0000

    leadboard ranking number

--- public/javascripts/leaderboard.js
+++ public/javascripts/leaderboard.js
@@ -8,14 +8,19 @@ window.onload = async function() {
 
         let html = "";
         let html2 = "";
-
+        let html3 = "";
+        let number = 0;
          for (let user of users) {
+          number += 1;
           html+=`${user.username} <p>`
           html2+=`${user.Pontos} <p>`
+          html3+=`${number}º<p>`
+
         }
         console.log(html)
         document.getElementById("username").innerHTML = html;
         document.getElementById("pontos").innerHTML = html2;
+        document.getElementById("number").innerHTML = html3;
 
       } catch (err) {
         console.log(err);
